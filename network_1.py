b9260c0d6e4af98f34981d8f7e1c060b03f2a2e4
Keely Weisbeck <keelyweisbeck@Keelys-MacBook-Pro.local>
2018-11-27 15:22:20 -0700

    part 1 section a complete

--- network_1.py
+++ network_1.py
@@ -127,6 +127,27 @@ class Host:
 
 
 ## Implements a multi-interface router
+class MPLSFrame():
+    label_S_len = 10
+
+    def __init__(self, packet, label_S):
+        self.packet = packet
+        self.label_S = label_S
+
+    def __str__(self):
+        return self.to_byte_S()
+
+    def to_byte_S(self):
+        byte_S = self.label_S.zfill(self.label_S_len)
+        byte_S += self.packet
+        return byte_S
+
+    def from_byte_S(self, byte_S):
+        label_S = byte_S[0: self.label_S_len].lstrip('0')
+        packet = byte_S[self.label_S_len:]
+        return self(packet, label_S)
+
+
 class Router:
 
     ##@param name: friendly router name for debugging
@@ -166,9 +187,9 @@ class Router:
                 self.process_network_packet(p, i)
             elif fr.type_S == "MPLS":
                 # TODO: handle MPLS frames
-                # m_fr = MPLSFrame.from_byte_S(pkt_S) #parse a frame out
+                m_fr = MPLSFrame.from_byte_S(pkt_S)  # parse a frame out
                 # for now, we just relabel the packet as an MPLS frame without encapsulation
-                m_fr = p
+                #m_fr = p
                 # send the MPLS frame for processing
                 self.process_MPLS_frame(m_fr, i)
             else:
@@ -179,11 +200,15 @@ class Router:
     #  @param i Incoming interface number for packet p
     def process_network_packet(self, pkt, i):
         # TODO: encapsulate the packet in an MPLS frame based on self.encap_tbl_D
-        # for now, we just relabel the packet as an MPLS frame without encapsulation
-        m_fr = pkt
-        print('%s: encapsulated packet "%s" as MPLS frame "%s"' % (self, pkt, m_fr))
-        # send the encapsulated packet for processing as MPLS frame
-        self.process_MPLS_frame(m_fr, i)
+        e = self.encap_tbl_D.get(pkt.dst)  # packet destination
+        print("TEST")
+        print(e)
+        if e is not None:  # if destination is in encap table
+            m_fr = MPLSFrame(pkt.to_byte_S(), e)  # encap it
+            print('%s: encapsulated packet "%s" as MPLS frame "%s"' % (self, pkt, m_fr))
+
+            # send encap packet
+            self.process_MPLS_frame(m_fr, i)
 
     ## process an MPLS frame incoming to this router
     #  @param m_fr: MPLS frame to process
@@ -207,4 +232,4 @@ class Router:
             self.process_queues()
             if self.stop:
                 print(threading.currentThread().getName() + ': Ending')
-                return 
+                return
--- simulation_1.py
+++ simulation_1.py
@@ -19,9 +19,9 @@ if __name__ == '__main__':
     object_L.append(host_2)
 
     # create routers and routing tables for connected clients (subnets)
-    encap_tbl_D = {}  # table used to encapsulate network packets into MPLS frames
-    frwd_tbl_D = {}  # table used to forward MPLS frames
-    decap_tbl_D = {}  # table used to decapsulate network packets from MPLS frames
+    encap_tbl_D = {'H2' : '2'}  # table used to encapsulate network packets into MPLS frames
+    frwd_tbl_D = {(0, '2') : (1, '2'), (1, '1') : (0, '1')}  # table used to forward MPLS frames
+    decap_tbl_D = {'2' : 0}  # table used to decapsulate network packets from MPLS frames
     router_a = Router(name='RA',
                       intf_capacity_L=[500, 500],
                       encap_tbl_D=encap_tbl_D,
@@ -30,9 +30,9 @@ if __name__ == '__main__':
                       max_queue_size=router_queue_size)
     object_L.append(router_a)
 
-    encap_tbl_D = {}
-    frwd_tbl_D = {}
-    decap_tbl_D = {}
+    encap_tbl_D = {'H1' : '1'}
+    frwd_tbl_D = {(1, '1') : (0, '1'), (0, '2') : (1, '2')}
+    decap_tbl_D = {'1' : 1}
     router_b = Router(name='RB',
                       intf_capacity_L=[500, 100],
                       encap_tbl_D=encap_tbl_D,
