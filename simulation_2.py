9184c3172fd7cb7bd577e6ff46765673376c6e81
Keely26 <keelyweisbeck@gmail.com>
2018-12-10 11:16:50 -0700

    new routers in sim2

--- simulation_2.py
+++ simulation_2.py
@@ -19,27 +19,43 @@ if __name__ == '__main__':
     object_L.append(host_2)
 
     # create routers and routing tables for connected clients (subnets)
-    encap_tbl_D = {}  # table used to encapsulate network packets into MPLS frames
-    frwd_tbl_D = {}  # table used to forward MPLS frames
-    decap_tbl_D = {}  # table used to decapsulate network packets from MPLS frames
+    frwd_tbl_D = {(0, '5') : (1, '5'), (1, '6') : (0, '6')}
+    decap_tbl_D = {'6': 1}
     router_a = Router(name='RA',
-                      intf_capacity_L=[500, 500],
+                      intf_capacity_L=[500, 500, 500, 500],
                       encap_tbl_D=encap_tbl_D,
                       frwd_tbl_D=frwd_tbl_D,
                       decap_tbl_D=decap_tbl_D,
                       max_queue_size=router_queue_size)
     object_L.append(router_a)
 
-    encap_tbl_D = {}
-    frwd_tbl_D = {}
-    decap_tbl_D = {}
+    encap_tbl_D = {'H1': '6'}  # table used to encapsulate network packets into MPLS frames
+    frwd_tbl_D = {(1, '6') : (0, '6'), (0, '5') : (1, '5')}
+    decap_tbl_D = {'5': 1}
     router_b = Router(name='RB',
-                      intf_capacity_L=[500, 100],
+                      intf_capacity_L=[500, 500],
                       encap_tbl_D=encap_tbl_D,
                       frwd_tbl_D=frwd_tbl_D,
                       decap_tbl_D=decap_tbl_D,
                       max_queue_size=router_queue_size)
     object_L.append(router_b)
+
+
+    router_c = Router(name='RC',
+                      intf_capacity_L=[500, 500],
+                      encap_tbl_D=encap_tbl_D,
+                      frwd_tbl_D=frwd_tbl_D,
+                      decap_tbl_D=decap_tbl_D,
+                      max_queue_size=router_queue_size)
+    object_L.append(router_c)
+
+    router_d = Router(name='RD',
+                      intf_capacity_L=[500, 500, 500],
+                      encap_tbl_D=encap_tbl_D,
+                      frwd_tbl_D=frwd_tbl_D,
+                      decap_tbl_D=decap_tbl_D,
+                      max_queue_size=router_queue_size)
+    object_L.append(router_d)
 
     # create a Link Layer to keep track of links between network nodes
     link_layer = LinkLayer()
